7c85217870962c3912e4b9c80c5aff1801e3d004
snake1ekans <smithga3@proton.me>
2026-09-09 20:20:49 -0600

    Oops I was testing on prod...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        var tester = new Sampleclass();
-        System.out.println(tester.name);
-        tester.person = "gavin";
-        System.out.println(tester.person);
+
     }
 }
--- Sampleclass.java
+++ Sampleclass.java
@@ -1,6 +1,8 @@
 public class Sampleclass {
     public String name = "test";
     public String person;
+    int[] numbers = {0,1,2};
+    public static int samplesize = 3;
     
 
 }
