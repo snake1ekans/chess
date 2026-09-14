a01bcf983de3dd499ffe3f252a333bd504530561
snake1ekans <smithga3@proton.me>
2026-09-14 09:33:16 -0600

    Added type and color functions

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -26,20 +26,22 @@ public class ChessPiece {
     }
 
     private ChessPosition position;
+    private ChessGame.TeamColor team;
+    private PieceType type;
 
 
     /**
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return team;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
