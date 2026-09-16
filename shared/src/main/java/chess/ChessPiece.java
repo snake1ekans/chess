1ec0b2a87a30d2374b64590b9e3d0c5108d0ab68
snake1ekans <smithga3@proton.me>
2026-09-15 20:21:20 -0600

    Got King tests to pass. Seems to have passed all other tests, but other piece logic needs implementation. Hanuman - Rodrigo y Gabriela

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -11,6 +11,7 @@ import java.util.*;
 public class ChessPiece {
     final ChessGame.TeamColor team;
     final ChessPiece.PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.team = pieceColor;
         this.type = type;
@@ -52,39 +53,48 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         List<ChessMove> validMoves = new ArrayList<ChessMove>();
             ChessPiece piece = board.getPiece(myPosition);
+
             if (piece.getPieceType() == PieceType.KNIGHT) {return knightMove(validMoves, board);}
-            if (piece.getPieceType() == PieceType.KING) {return kingMove(validMoves, board);}
+            if (piece.getPieceType() == PieceType.KING) {return kingMove(validMoves, board, myPosition);}
 
         return validMoves;
     }
 
-    private List<ChessMove> kingMove(List<ChessMove> validMoves, ChessBoard board) {
+    private List<ChessMove> kingMove(List<ChessMove> validMoves, ChessBoard board, ChessPosition position) {
         int row = position.getRow();
         int col = position.getColumn();
 
-        if (col-1>0 && row-1>0 && board.getPiece(new ChessPosition(row-1, col-1)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row-1, col-1), null));}
+        ChessPosition downLeft = new ChessPosition(row-1, col-1);
+        if (col-1>0 && row-1>0 && (board.getPiece(downLeft) == null || board.getPiece(downLeft).team != this.team)) {
+            validMoves.add(new ChessMove(position, downLeft, null));}
 
-        if (col-1>0 && board.getPiece(new ChessPosition(row, col-1)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row, col-1), null));}
+        ChessPosition down =new ChessPosition(row, col-1);
+        if (col-1>0 && (board.getPiece(down) == null || board.getPiece(down).team != this.team)) {
+            validMoves.add(new ChessMove(position, down, null));}
 
-        if (col-1>0 && row+1 <=8 && board.getPiece(new ChessPosition(row+1, col-1)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row+1, col-1), null));}
+        ChessPosition downRight = new ChessPosition(row+1, col-1);
+        if (col-1>0 && row+1 <=8 && (board.getPiece(downRight) == null || board.getPiece(downRight).team != this.team)) {
+            validMoves.add(new ChessMove(position, downRight, null));}
 
-        if (col+1<=8 && row-1>0 && board.getPiece(new ChessPosition(row-1, col+1)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row-1, col+1), null));}
+        ChessPosition upLeft = new ChessPosition(row-1, col+1);
+        if (col+1<=8 && row-1>0 && (board.getPiece(upLeft) == null || board.getPiece(upLeft).team != this.team)) {
+            validMoves.add(new ChessMove(position, upLeft, null));}
 
-        if (col+1<=8 && board.getPiece(new ChessPosition(row, col+1)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row, col+1), null));}
+        ChessPosition up =new ChessPosition(row, col+1);
+        if (col+1<=8 && (board.getPiece(up) == null || board.getPiece(up).team != this.team)) {
+            validMoves.add(new ChessMove(position, up, null));}
 
-        if (col+1<=8 && row+1 <=8 && board.getPiece(new ChessPosition(row+1, col+1)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row+1, col+1), null));}
+        ChessPosition upRight =new ChessPosition(row+1, col+1);
+        if (col+1<=8 && row+1 <=8 && (board.getPiece(upRight) == null || board.getPiece(upRight).team != this.team)) {
+            validMoves.add(new ChessMove(position, upRight, null));}
 
-        if (row+1 <=8 && board.getPiece(new ChessPosition(row+1, col)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row+1, col), null));}
+        ChessPosition right = new ChessPosition(row+1, col);
+        if (row+1 <=8 && (board.getPiece(right) == null || board.getPiece(right).team != this.team)) {
+            validMoves.add(new ChessMove(position, right, null));}
 
-        if (row-1>0 && board.getPiece(new ChessPosition(row-1, col)).team != this.team) {
-            validMoves.add(new ChessMove(position, new ChessPosition(row-1, col), null));}
+        ChessPosition left = new ChessPosition(row-1, col);
+        if (row-1>0 && (board.getPiece(left) == null || board.getPiece(left).team != this.team)) {
+            validMoves.add(new ChessMove(position, left, null));}
         return validMoves;
     }
 
